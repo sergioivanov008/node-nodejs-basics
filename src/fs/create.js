81dbc13a5741752d1b11ed8aa063a560d9770629
sergioivanov008 <sergioivanov008@gmail.com>
2022-12-05 01:14:42 +0300

    feat: implement function create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { access, constants } from 'fs';
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const curFolder = 'files';
+const curFile = 'fresh.txt';
+const curContent = 'I am fresh and young';
+
+const curFilePath = path.join(__dirname, curFolder, curFile);
+
 const create = async () => {
-    // Write your code here 
+        access(curFilePath, constants.R_OK, (err) => {
+            if (err) {
+                writeFile(curFilePath, curContent);
+            } else {
+                throw new Error('FS operation failed');
+            }
+          });
 };
 
 await create();
